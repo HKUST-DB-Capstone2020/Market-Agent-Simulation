e52b4c747df9b91cb414142cc752d4d2a356a131
Martingale <34835504+wangboyu15@users.noreply.github.com>
2020-04-13 20:54:40 +0800

    Update OMS.py

--- OMS/OMS.py
+++ OMS/OMS.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.pardir)
 from LOB.LOB import LimitOrderBook
 
 class action():
-        def __init__(self, input_action):
+    def __init__(self, input_action):
             self.agent, self.type, self.direction, self.quantity, self.price = input_action
 #             self.agent = input_action[0]
 #             self.type = input_action[1]

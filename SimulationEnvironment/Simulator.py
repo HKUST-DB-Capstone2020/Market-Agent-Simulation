e81cd0e6603ea8fc96771b5efb573375ab05e5ec
Martingale <34835504+wangboyu15@users.noreply.github.com>
2020-06-12 22:37:11 +0800

    Incorporated multiple actions the strategy returns

--- SimulationEnvironment/Simulator.py
+++ SimulationEnvironment/Simulator.py
@@ -105,8 +105,9 @@ class Simulator:
     
     def step(self, action):
         
-        if action: # action is not empty list
-            self.OMSTest.receive(action)
+        for act in action:  # multiple actions
+            if act:  # this action is not empty list
+                self.OMSTest.receive(action)
         
         self.state.lastTime = self.state.current_time
         self.ZIAgent.Execute(self.OMSTest)              # execute ZIAgent generator
